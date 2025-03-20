ee7ea56dd5f632b21cf240b30ba6c000e4c73b4c
pariionme <prewa.choteboon@gmail.com>
2025-03-20 16:15:36 +0700

    Adding new ecommerce app and item page

--- general/views.py
+++ general/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def ecommerce _index_view(request):
+    '''This Function render index page of ecomerce views'''
+
+    return HttpResponse('Welcome to CN334 Praewa 6610742162 views!')
